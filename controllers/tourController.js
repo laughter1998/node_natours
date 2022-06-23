7fe14343d7df136b57b99444b7f763a9b0e91f91
laughter1998 <laughter98@hanmail.net>
2022-06-23 15:01:44 +0900

    90. Reading Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { findById } = require('../models/tourModel');
 const Tour = require('../models/tourModel');
 
 // const tours = JSON.parse(
@@ -19,35 +20,57 @@ const Tour = require('../models/tourModel');
 //     }
 //     next();
 // }
-exports.checkBody = (req, res, next)=>{
-    if(!req.body.name || !req.body.price){
-        return res
-        .status(400)
-        .json({
-            status: 'fail',
-            message:"Missing name or price"
-        })
-    }
-    next();
-}
+// exports.checkBody = (req, res, next)=>{
+//     if(!req.body.name || !req.body.price){
+//         return res
+//         .status(400)
+//         .json({
+//             status: 'fail',
+//             message:"Missing name or price"
+//         })
+//     }
+//     next();
+// }
 
-exports.getAllTours = (req, res)=>{
-    console.log(req.requestTime);
-    res.status(200).json({
-        status : 'success',
-        requestedAt: req.requestTime,
-        // results: tours.length,
-        // data : {
-        //     tours
-        // }
-    });
+exports.getAllTours = async (req, res)=>{
+    // console.log(req.requestTime);
+    try{
+
+        const tours = await Tour.find();
+        res.status(200).json({
+            status : 'success',
+            // requestedAt: req.requestTime,
+            results: tours.length,
+            data : {
+                tours
+            }
+        });
+    } catch (err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 
-exports.getTour = (req, res)=>{
-    console.log(req.params);
+exports.getTour = async (req, res)=>{
+    // console.log(req.params);
 
-    const id = req.params.id * 1;
-
+    const id = req.params.id ;
+    try {
+        const tour = await Tour.findById(id);
+        res.status(200).json({
+            status: 'success',
+            data : {
+                tours: tour
+            }
+        })
+    } catch(err){
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
     // const tourLen = tours.length;
     // const tour = tours.find( el=> el.id === id);
     
@@ -61,14 +84,26 @@ exports.getTour = (req, res)=>{
     // });
 }
 
-exports.createTour = (req, res)=>{
-    res.status(201).json({
-        status: 'success',
-        // data: {
-        //     tour: newTour
-        // }
-    });
-    //  console.log(req.body.id);
+exports.createTour = async (req, res)=>{
+    try {
+        // const newTour = new Tour({});
+        // newTour.save()
+        const nowTour = await Tour.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: nowTour
+            }
+        });
+        //  console.log(req.body.id);
+    } catch (err){
+        res.status(400).json({
+            status: 'fail',
+            message: 'Invalid data sent!'
+        })
+    }
+    
     
    
     // const newId = tours[tours.length - 1].id + 1;
