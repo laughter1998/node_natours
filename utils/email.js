162de62f1463b5cab63d2abfeda0ddd026c08eb2
laughter1998 <laughter98@hanmail.net>
2023-01-03 09:54:59 +0900

    커밋

--- utils/email.js
+++ utils/email.js
@@ -3,12 +3,13 @@ const nodemailer = require('nodemailer');
 const sendEmail = options =>{
     // 1) 트랜스포터를 만들어야 합니다.
     const transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         auth: {
             user: process.env.EMAIL_USERNAME,
             password: process.env.EMAIL_PASSWORD
         }
-        // Gmail 계정에서 보안 수준이 낮은 앱 옵션이라는 항목을 실제로 활성화해
+        // Gmail 계정이라면 보안 수준이 낮은 앱 옵션이라는 항목을 실제로 활성화해
     })
     // 2) 이메일 옵션을 정의
 
